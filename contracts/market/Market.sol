6a885c744daa725b7c100c1a95443dda5053b352
Michael Wang <michaelwang0213@yahoo.com>
2019-07-12 13:46:38 -0400

    made stuff public

--- contracts/market/Market.sol
+++ contracts/market/Market.sol
@@ -10,27 +10,57 @@ contract Market {
 
 
   using SafeMath for uint256;
-  address main; // main market address
+  
+  //address public main; // main market address
+  
   ZapToken public zapToken;
   ZapCoordinatorInterface public coordinator;
-  address fixsupplyaddress;
+ 
+  address public fixsupplyaddress;
   FixedSupplyToken public FToken;
-  uint256  public buyPrice ;
+  uint256  public buyPrice;
+  
+  address public oracleAddress; // address of account operating oracle
 
-  constructor(address _coordinator) public {
+  event GetPriceEvent();
+  event PriceUpdated(uint256 amount);
+
+  constructor(address _coordinator, address _oracle) public {
       coordinator = ZapCoordinatorInterface(_coordinator);
       address zapTokenAddress = coordinator.getContract("ZAP_TOKEN");
-      /* main= coordinator.getContract("MainMarket"); */
+      // main= coordinator.getContract("MainMarket");
       fixsupplyaddress = coordinator.getContract("FixedSupplyToken");
       FToken= FixedSupplyToken(fixsupplyaddress);
       zapToken = ZapToken(zapTokenAddress);
-
+      oracleAddress = _oracle;
   }
 
 //returns the current price of zap to token
-  function getPrice () public pure returns (uint256){
+  function getPrice () public returns (uint256){
   //e/btc divided e/zapwe
-    return 2; //need to change to match oracle
+    emit GetPriceEvent();
+    return uint8(uint256(keccak256(block.timestamp, block.difficulty))%131) * 1000000000000000 + 100000000000000000 ; //need to change to match oracle
+  }
+
+//return the number of token a person has
+  function getTokenBalance (address _user) public view returns (uint256){
+    return FToken.balanceOf(_user) ;
+  }
+
+// 
+  function getZapWeiSpent(address _user) public view returns (uint256) {
+    return FToken.zapWei_spent(_user);
+  }
+
+  function getZapBalance (address _user) public view returns (uint256) {
+    return zapToken.balanceOf(_user);
+  }
+
+// funtion oracle calls to update the price
+  function updatePrice (uint _price) public {
+    require(msg.sender == oracleAddress);
+    buyPrice = _price;
+    emit PriceUpdated(_price);
   }
 
 //buying token
@@ -38,51 +68,51 @@ contract Market {
 
     buyPrice = getPrice();//set it to the new price using oracle ;
     uint256 zaprequire = numberoftoken.mul(buyPrice);
-  //check if msg.sender has enough zap
+  //check if msg.sender has enoughg
     zapToken.approve(address(this),zaprequire); //address _spender, uint256 _valu
+
+
     zapToken.transferFrom(msg.sender,address(this),zaprequire);  //transferFrom(address from, address to, uint256 value)
     FToken.transfer(msg.sender,numberoftoken); // sends token to zap
 
-      return numberoftoken;
-    }
-
-
-//return the number of token a person has
-  function getBalance () public view returns (uint256){
-    return FToken.balanceOf(msg.sender) ;
+    return numberoftoken;
   }
 
 //withrawn an amount of Token
-  function Withdrawn (uint256 withdraw_amount) public payable returns (string memory ) {
+  function Withdrawn (uint256 withdraw_amount) public payable returns (uint8) {
     if (FToken.balanceOf(msg.sender) >= withdraw_amount){ //check if msg.sender has enough token
-    buyPrice= getPrice();// set the buy price by usign oracle
-    uint256 previousprice= (withdraw_amount.mul(FToken.zapWei_spent(msg.sender))).div(FToken.balanceOf(msg.sender));
-    uint256 currentprice = withdraw_amount.mul(buyPrice) ;
-    uint256 withdrawfee =  (currentprice.mul(15)).div(100); //15% fee
-    if (currentprice >= previousprice){ //user wins
-      uint256 difference = currentprice.sub( previousprice);//somehow send this to the main to tell them to pay the different
-      zapToken.transfer(msg.sender,previousprice);
+      buyPrice= getPrice();// set the buy price by usign oracle
+      uint256 previousprice= (withdraw_amount.mul(FToken.zapWei_spent(msg.sender))).div(FToken.balanceOf(msg.sender));
+      uint256 currentprice = withdraw_amount.mul(buyPrice);
+      //uint256 withdrawfee =  (currentprice.mul(15)).div(100); //15% fee
+        uint256 payment;
+      if (currentprice >= previousprice){ //user wins
+        uint256 difference = currentprice.sub(previousprice);//somehow send this to the main to tell them to pay the different
+        
+        // call main withdraw function
+        // ask main for difference minus tax
 
-      zapToken.transferFrom(main,msg.sender,difference);
+        payment = /*withdraw(difference)*/ buyPrice;
+        
 
-    }
-    else {//main market win
-      uint256 gain= previousprice.sub(currentprice);
-      zapToken.transfer(msg.sender,currentprice);
-      zapToken.transferFrom(address(this),main,gain);
+      }
+      else {//main market win
+        uint256 gain= previousprice.sub(currentprice);
 
+        // call main withdraw function
+        // give main difference plus tax
 
+        payment = /*withdraw(difference)*/ buyPrice;
+      }
 
-    }
-    FToken.approve(address(this),withdraw_amount);
-    FToken.transferFrom(msg.sender, fixsupplyaddress,withdraw_amount);//(address from, address to, uint tokens)
-    zapToken.transferFrom(msg.sender,main,withdrawfee);
+      FToken.approve(address(this),withdraw_amount);
+      FToken.transferFrom(msg.sender, address(this), withdraw_amount);//(address from, address to, uint tokens)
+      zapToken.transfer(msg.sender, payment);
 
-    return "withdraw complete. ";
-    }
-
+      return 1;
+    } 
     else{
-      return "not a valid amount to withdraw";
+      return 0;
     }
   }
 }
